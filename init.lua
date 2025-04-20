ca6819299bf336c4446487d6c5527edeb81ca88b
Aaron <aaronvegoda@gmail.com>
2025-04-20 17:29:04 +0100

    Add alias for save and res sessions

--- init.lua
+++ init.lua
@@ -126,6 +126,19 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Rebind Commands
+
+vim.api.nvim_create_user_command(
+   'Sessave',
+   ':SessionSave',
+   { desc = "Same as ':SessionSave'", force = false }
+)
+vim.api.nvim_create_user_command(
+   'Sesres',
+   ':SessionRestore',
+   { desc = "Same as ':SessionRestore'", force = false }
+)
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
